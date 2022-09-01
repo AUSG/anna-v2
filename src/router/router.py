b8eb859654ae95b457b22ec62e1738e1e8bf0e54
Roeniss Moon <roeniss2@gmail.com>
2022-09-02 04:32:03 +0900

    에러 발생 시 해당 채널에 새 글을 쓰는 대신 관련 쓰레드에 댓글 형태로 출력 (resolve #8)

--- src/router/router.py
+++ src/router/router.py
@@ -5,9 +5,18 @@ from typing import List, Callable, Any, Dict
 from slack_bolt import App, Ack, Say
 from slack_sdk import WebClient
 
+from util import get_prop
+
 logger = logging.getLogger(__name__)
 
 _SERVICES = []
+
+
+def _get_ts(event):
+    if get_prop(event, 'item', 'ts') is not None:
+        return get_prop(event, 'item', 'ts')
+    else:
+        return get_prop(event, 'item', 'thread_ts')
 
 
 def _call_services(ack: Ack, event: Dict[str, Any], say: Say, web_client: WebClient):
@@ -18,7 +27,7 @@ def _call_services(ack: Ack, event: Dict[str, Any], say: Say, web_client: WebCli
         except BaseException as ex:
             tb = traceback.format_exc()
             logger.error(f"{ex} ({tb})")
-            say(text=f"예상치 못한 에러가 발생했어! ({ex})")
+            say(text=f"예상치 못한 에러가 발생했어! ({ex})", thread_ts=_get_ts(event))
 
 
 def listen_event_with_services(app: App, services: List[Callable]):
