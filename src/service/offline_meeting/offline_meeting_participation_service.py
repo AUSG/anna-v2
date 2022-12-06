a5a5d3d276f5b3cde9b4a2760ebda3b614e9003e
mokhs00 <mokhs00@naver.com>
2022-12-06 22:19:04 +0900

    Refactor WORKSHEET_ID_CACHE 를 전역변수로 변경

--- src/service/offline_meeting/offline_meeting_participation_service.py
+++ src/service/offline_meeting/offline_meeting_participation_service.py
@@ -6,7 +6,6 @@ from slack_bolt import Say
 from slack_sdk import WebClient
 
 from expiringdict import ExpiringDict
-
 from implementation import GoogleSpreadsheetClient, SlackClient
 from util import get_prop, SlackGeneralEvent
 from .action_commander import ActionCommander, TargetEvent, RejectCondition, ActionCommand
@@ -22,6 +21,7 @@ MEMBERS_INFO_WORKSHEET_ID = int(os.environ.get('MEMBERS_INFO_WORKSHEET_ID'))
 FORM_SPREADSHEET_ID = os.environ.get('FORM_SPREADSHEET_ID')
 
 ONE_WEEK = 60 * 60 * 24 * 7
+WORKSHEET_ID_CACHE = ExpiringDict(max_len=100, max_age_seconds=ONE_WEEK)
 
 @dataclass
 class EmojiAddedEvent:
@@ -36,20 +36,24 @@ def participate_offline_meeting(event: SlackGeneralEvent, say: Say, web_client: 
     gs_client = GoogleSpreadsheetClient(slack_client)
     member_finder = MemberFinder(gs_client)
     worksheet_maker = WorksheetMaker(slack_client, gs_client)
-    service = OfflineMeetingParticipationService(event, action_commander, slack_client, gs_client, member_finder, worksheet_maker)
+    service = OfflineMeetingParticipationService(event, action_commander, slack_client, gs_client, member_finder,
+                                                 worksheet_maker, WORKSHEET_ID_CACHE)
 
     service.run()
 
 
 class OfflineMeetingParticipationService:
-    def __init__(self, raw_event: SlackGeneralEvent, action_commander: ActionCommander, slack_client: SlackClient, gs_client: GoogleSpreadsheetClient, member_finder: MemberFinder, worksheet_maker: WorksheetMaker):
-        self.event = EmojiAddedEvent(get_prop(raw_event, 'item', 'ts'), get_prop(raw_event, 'item', 'channel'), get_prop(raw_event, 'user'))
+    def __init__(self, raw_event: SlackGeneralEvent, action_commander: ActionCommander, slack_client: SlackClient,
+                 gs_client: GoogleSpreadsheetClient, member_finder: MemberFinder, worksheet_maker: WorksheetMaker,
+                 is_exist_worksheet_cache: ExpiringDict):
+        self.event = EmojiAddedEvent(get_prop(raw_event, 'item', 'ts'), get_prop(raw_event, 'item', 'channel'),
+                                     get_prop(raw_event, 'user'))
         self.action_commander = action_commander
         self.slack_client = slack_client
         self.gs_client = gs_client
         self.member_finder = member_finder
         self.worksheet_maker = worksheet_maker
-        self.is_exist_worksheet_cache = ExpiringDict(max_len=100, max_age_seconds=ONE_WEEK)
+        self.is_exist_worksheet_cache = is_exist_worksheet_cache
 
     def run(self):
         if not self._is_target_event():
@@ -68,7 +72,8 @@ class OfflineMeetingParticipationService:
         if command == ActionCommand.NOOP:
             return False
         elif command == ActionCommand.REJECT:
-            self.slack_client.tell(msg=f"오거나이저가 :stop2: 이모지를 붙여놨기 때문에 <@{self.event.user}>의 요청을 들어줄 수가 없어.", ts=self.event.ts)
+            self.slack_client.tell(msg=f"오거나이저가 :stop2: 이모지를 붙여놨기 때문에 <@{self.event.user}>의 요청을 들어줄 수가 없어.",
+                                   ts=self.event.ts)
             return False
         else:
             return True
@@ -87,11 +92,14 @@ class OfflineMeetingParticipationService:
 
             self.gs_client.append_row(FORM_SPREADSHEET_ID, worksheet_id, member.to_list())
             if is_new and not self.is_exist_worksheet_cache.get(worksheet_id):
-                self.slack_client.tell(msg=f"새로운 시트를 만들었어! <{self.gs_client.get_url(FORM_SPREADSHEET_ID, worksheet_id)}|구글스프레드 시트>", ts=self.event.ts)
+                self.slack_client.tell(
+                    msg=f"새로운 시트를 만들었어! <{self.gs_client.get_url(FORM_SPREADSHEET_ID, worksheet_id)}|구글스프레드 시트>",
+                    ts=self.event.ts)
 
                 self.is_exist_worksheet_cache[worksheet_id] = True
 
             self.slack_client.tell(msg=f"<@{self.event.user}>, 등록 완료!", ts=self.event.ts)
+
         except Exception as e:
             logging.error("fail OfflineMeetingParticipationService._participate()")
             raise e
