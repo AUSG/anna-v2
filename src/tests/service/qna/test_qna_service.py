17af29cd15870df932432c0a64f1d5c03c64f387
Roeniss Moon <roeniss2@gmail.com>
2022-12-13 13:03:04 +0900

    qna_service 테스트는 한 폴더에 모음

--- src/tests/service/qna/test_qna_service.py
+++ src/tests/service/qna/test_qna_service.py
@@ -1,4 +1,6 @@
-from service.qna.qna_service import _make_question, _make_dictionary, _find_answer, _tell_answer, _reply_to_question_v2
+from _pytest.python_api import raises
+
+from service.qna.qna_service import _make_question, _make_dictionary, _find_answer, _tell_answer, _reply_to_question_v2, _find_keyword
 from tests.util.mock_slack_client import MockSlackClient
 
 
@@ -95,3 +97,43 @@ def test_reply_to_question_v2():
     slack_client = MockSlackClient()
     _reply_to_question_v2(event, slack_client)
     assert slack_client.is_tell_called is True
+
+
+###
+
+
+def test_fail_when_keyword_prefix_is_None():
+    with raises(TypeError):
+        _find_keyword(None, '!random string')
+
+
+def test_fail_when_text_is_None():
+    assert _find_keyword('!', None) is None
+
+
+def test_fail_when_keyword_prefix_not_in_text():
+    assert _find_keyword('!', 'random string') is None
+
+
+def test_fail_when_keyword_prefix_on_last_position_in_text():
+    assert _find_keyword('!', 'random string!') is None
+
+
+def test_fail_keyword_is_only_spaces():
+    assert _find_keyword('!', '!          ') is None
+
+
+def test_success_with_one_length_keyword_prefix():
+    assert _find_keyword('!', 'string!random hello') == 'random'
+
+
+def test_success_with_two_length_keyword_prefix():
+    assert _find_keyword('!!', 'str!!ingrandom hello') == 'ingrandom'
+
+
+def test_success_with_keyword_prefix_in_two_position():
+    assert _find_keyword('!', 'str!ing!random h!ello') == 'ing'
+
+
+def test_success_with_two_length_keyword_prefix_in_two_position():
+    assert _find_keyword('!!', 'str!!ingrand!!om he!!llo') == 'ingrand'
