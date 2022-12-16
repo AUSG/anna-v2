d47bf8dd618fe595be46541fa339981b56050915
Roeniss Moon <roeniss2@gmail.com>
2022-12-17 03:12:53 +0900

    pytest-mock 추가, 테스트를 한글로 변경

--- src/tests/service/qna/test_qna_service.py
+++ src/tests/service/qna/test_qna_service.py
@@ -1,41 +1,43 @@
 from _pytest.python_api import raises
 
+from implementation import SlackClient
 from service.qna.qna_service import _make_question, _make_dictionary, _find_answer, _tell_answer, _reply_to_question_v2, _find_keyword
-from tests.util.mock_slack_client import MockSlackClient
 
 
 class Test__make_question:
-    def success_with_thread_ts_and_type_and_text(self):
+    def test_스레드에_남긴_댓글의_이벤트는_ts와_text를_사용해_ts와_keyword를_리턴한다(self):
         event = {'type': 'message', 'thread_ts': '123.45', 'text': '!hey'}
         keyword, ts = _make_question(event)
         assert keyword == 'hey'
         assert ts == '123.45'
 
-    def success_with_ts_and_type_and_text(self):
+    def test_채널에_남긴_댓글의_이벤트는_ts와_text를_사용해_ts와_keyword를_리턴한다(self):
         event = {'type': 'message', 'ts': '123.45', 'text': '!hey'}
         keyword, ts = _make_question(event)
         assert keyword == 'hey'
         assert ts == '123.45'
 
-    def success_with_ts_and_thread_ts_and_type_and_text(self):
+    def test_thread_ts와_ts와_text_가_주어지면_ts_인풋은_무시한다(self):
         event = {'type': 'message', 'ts': '123.45', 'thread_ts': '678.90', 'text': '!hey'}
+
         keyword, ts = _make_question(event)
+
         assert keyword == 'hey'
         assert ts == '678.90'
 
-    def success_without_ts_and_thread_ts(self):
+    def test_ts와_thread_ts가_없다면_keyword와_ts을_리턴하지_않는다(self):
         event = {'type': 'message', 'text': '!hey'}
         keyword, ts = _make_question(event)
         assert keyword is None
         assert ts is None
 
-    def success_without_type(self):
+    def test_메시지_type이_없다면_keyword와_ts을_리턴하지_않는다(self):
         event = {'thread_ts': '123.45', 'text': '!hey'}
         keyword, ts = _make_question(event)
         assert keyword is None
         assert ts is None
 
-    def success_without_keyword(self):
+    def test_키워드를_생성하지_못하면_keyword와_ts을_리턴하지_않는다(self):
         event = {'type': 'message', 'ts': '123.45', 'text': '!'}
         keyword, ts = _make_question(event)
         assert keyword is None
@@ -49,72 +51,90 @@ class Test__make_dictionary:
 
 
 class Test__find_answer:
-    def success_when_keyword_exists_in_dictionary(self):
+    def test_when_keyword_exists_in_dictionary(self):
         dictionary = {"question": "answer"}
         answer = _find_answer(dictionary, "question")
         assert answer == "answer"
 
-    def success_when_keyword_not_exists_in_dictionary(self):
+    def test_when_keyword_not_exists_in_dictionary(self):
         dictionary = {"question": "answer"}
         answer = _find_answer(dictionary, None)
         assert answer is None
 
-    def success_when_dictionary_is_empty(self):
+    def test_when_dictionary_is_empty(self):
         dictionary = {}
         answer = _find_answer(dictionary, "question")
         assert answer is None
 
 
 class Test__tell_answer:
-    def success_with_text_and_ts(self):
-        slack_client = MockSlackClient()
-        _tell_answer(slack_client, "hello", "123.45")
-        assert slack_client.is_tell_called is True
+    def test_with_text_and_ts(self, mocker):
+        mock_slack_client = self._make_mock_slack_client(mocker)
 
-    def success_without_text(self):
-        slack_client = MockSlackClient()
-        _tell_answer(slack_client, None, "123.45")
-        assert slack_client.is_tell_called is False
+        _tell_answer(mock_slack_client, "hello", "123.45")
 
-    def success_without_ts(self):
-        slack_client = MockSlackClient()
-        _tell_answer(slack_client, "hello", None)
-        assert slack_client.is_tell_called is False
+        mock_slack_client.tell.assert_called()
+
+    def test_without_text(self, mocker):
+        mock_slack_client = self._make_mock_slack_client(mocker)
+
+        _tell_answer(mock_slack_client, None, "123.45")
+
+        mock_slack_client.tell.assert_not_called()
+
+    def test_without_ts(self, mocker):
+        mock_slack_client = self._make_mock_slack_client(mocker)
+
+        _tell_answer(mock_slack_client, "hello", None)
+
+        mock_slack_client.tell.assert_not_called()
+
+    def _make_mock_slack_client(self, mocker):
+        mocker.patch.object(SlackClient, "tell")
+        mock_slack_client = SlackClient("dummy", "dummy")
+        return mock_slack_client
 
 
 class Test__find_keyword:
-    def fail_with_keyword_prefix_is_None(self):
+    def test_with_keyword_prefix_is_None(self):
         with raises(TypeError):
             _find_keyword(None, '!random string')
 
-    def fail_with_text_is_None(self):
+    def test_with_text_is_None(self):
         assert _find_keyword('!', None) is None
 
-    def fail_with_keyword_prefix_not_in_text(self):
+    def test_with_keyword_prefix_not_in_text(self):
         assert _find_keyword('!', 'random string') is None
 
-    def fail_with_keyword_prefix_on_last_position_in_text(self):
+    def test_with_keyword_prefix_on_last_position_in_text(self):
         assert _find_keyword('!', 'random string!') is None
 
-    def fail_with_whitespaces_keyword(self):
+    def test_with_whitespaces_keyword(self):
         assert _find_keyword('!', '!          ') is None
 
-    def success_with_one_length_keyword_prefix(self):
+    def test_with_one_length_keyword_prefix(self):
         assert _find_keyword('!', 'string!random hello') == 'random'
 
-    def success_with_two_length_keyword_prefix(self):
+    def test_with_two_length_keyword_prefix(self):
         assert _find_keyword('!!', 'str!!ingrandom hello') == 'ingrandom'
 
-    def success_with_keyword_prefix_in_two_position(self):
+    def test_with_keyword_prefix_in_two_position(self):
         assert _find_keyword('!', 'str!ing!random h!ello') == 'ing'
 
-    def success_with_two_length_keyword_prefix_in_two_position(self):
+    def test_with_two_length_keyword_prefix_in_two_position(self):
         assert _find_keyword('!!', 'str!!ingrand!!om he!!llo') == 'ingrand'
 
 
 class Test__reply_to_question_v2:
-    def success_with_perfect_condition(self):
-        some_valid_event = {'type': 'message', 'thread_ts': '123.45', 'text': '!wifi'}
-        slack_client = MockSlackClient()
-        _reply_to_question_v2(event, slack_client)
-        assert slack_client.is_tell_called is True
+    def test_with_perfect_condition(self, mocker):
+        mock_slack_client = self._make_mock_slack_client(mocker)
+        event = {'type': 'message', 'thread_ts': '123.45', 'text': '!wifi'}
+
+        _reply_to_question_v2(event, mock_slack_client)
+
+        mock_slack_client.tell.assert_called()
+
+    def _make_mock_slack_client(self, mocker):
+        mocker.patch.object(SlackClient, "tell")
+        mock_slack_client = SlackClient("dummy", "dummy")
+        return mock_slack_client
--- src/tests/util/mock_slack_client.py
+++
@@ -1,8 +0,0 @@
-class MockSlackClient:
-    def __init__(self):
-        self.is_tell_called = False
-
-    def tell(self, text, thread_ts):
-        print(f"reply is called: text={text}, thread_ts={thread_ts}")
-        self.is_tell_called = True
-
