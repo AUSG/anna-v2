3b8c72b17f650969d31f5753332324727044881e
seonghyeok <seonghyeok@tosspayments.com>
2023-02-05 16:43:19 +0900

    remove feature: Thread Tracking (Archiving)

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,6 +1,4 @@
-from service.tracking.background_tracking_job import background_tracking_job
 from .qna import reply_to_question
 from .offline_meeting import participate_offline_meeting
-from .tracking.tracking_service import track_thread
 
-__all__ = ['reply_to_question', 'participate_offline_meeting', 'track_thread', 'background_tracking_job']
+__all__ = ['reply_to_question', 'participate_offline_meeting']
