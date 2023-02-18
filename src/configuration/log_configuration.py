e8813a73d6dc213c1de10275113e5334c9e5fa9a
Roeniss Moon <roeniss2@gmail.com>
2023-02-19 03:46:23 +0900

    LOCAL=True 일 때 모든 로그 출력

--- src/configuration/log_configuration.py
+++ src/configuration/log_configuration.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 def init_log():
     if Configs.LOCAL:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.NOTSET) # all messages
     else:
         logging.basicConfig(level=logging.INFO)
 
