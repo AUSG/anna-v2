d9f50b33da958dc34130146be9a5767a7cfa74c3
Hepheir <hepheir@gmail.com>
2024-08-01 19:11:46 +0900

    refactor: `strip_multipline` -> `textwrap.dedent`

--- src/handler/bigchat/join_bigchat.py
+++ src/handler/bigchat/join_bigchat.py
@@ -1,9 +1,10 @@
 from typing import List
 import re
+import textwrap
 
 from implementation.member_finder import MemberNotFound, MemberLackInfo
 from implementation.slack_client import Message
-from util.utils import strip_multiline
+
 
 SPREADSHEET_PAT = re.compile(
     r"https://docs.google.com/spreadsheets/d/.*/edit#gid=(\d*)"
@@ -60,7 +61,7 @@ class JoinBigchat:
 
         self.slack_client.send_message(msg=f"<@{self.user}>, 등록 완료!", ts=self.ts)
         self.slack_client.send_message_only_visible_to_user(
-            msg=strip_multiline(
+            msg=textwrap.dedent(
                 f"""
                 <@{self.user}> 네 신청 정보를 아래와 같이 등록했어. 바뀐 부분이 있다면 운영진에게 DM으로 알려줘!
                 ```
@@ -69,7 +70,8 @@ class JoinBigchat:
                 이메일: {member.email}
                 학교/회사: {member.school_name_or_company_name}
                 ```
-                (참고로 이 메시지는 너만 볼 수 있어!)"""
+                (참고로 이 메시지는 너만 볼 수 있어!)
+                """
             ),
             channel=self.channel,
             ts=self.ts,
