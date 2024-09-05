c016289ad669be1e1ab5d448c45436a7a0c1eb0b
Roeniss Moon <roeniss2@gmail.com>
2024-09-05 12:51:03 +0900

    feat: 셔플 기능 추가

--- src/handler/bigchat/simple_response.py
+++ src/handler/bigchat/simple_response.py
@@ -1,3 +1,7 @@
+import random
+import re
+
+
 class SimpleResponse:
     def __init__(self, event, slack_client):
         self.text = event["text"]
@@ -6,8 +10,19 @@ class SimpleResponse:
 
     def run(self):
         text = self.text.replace("<@U01BN035Y6L>", "").strip()
-        if text:
-            return False
-
-        self.slack_client.send_message(msg="?", ts=self.ts)
+        if self._is_shuffle_trigger(text):
+            shuffled = self._shuffle(text)
+            self.slack_client.send_message(msg=shuffled, ts=self.ts)
+        else:
+            self.slack_client.send_message(msg="?", ts=self.ts)
         return True
+
+    def _is_shuffle_trigger(self, text):
+        if "섞어줘" in text or "shuffle" in text.lower():
+            return True
+
+    def _shuffle(self, text):
+        # 유저 이름들을 정규표현식 (<@Uxxx>) 로 추출하고 셔플한 뒤 스트링화
+        users = re.findall(r'<@U[0-9a-zA-Z]+>', text)
+        random.shuffle(users)
+        return "여기있어!\n" + " ".join(users)
