3433ea71a2a667624d25065ba47955a609052266
kshired <shiroed1211@gmail.com>
2024-10-29 00:01:22 +0900

    test : add test

---
+++ test/handler/bigchat/test_mention_response.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import MagicMock
+
+from test.handler.bigchat.sample_data import create_sample_app_mention_event
+
+from handler.bigchat.shuffle_response import ShuffleResponse
+from handler.bigchat.simple_response import SimpleResponse
+from handler.bigchat.mention_response import MentionResponse
+
+
+class TestMentionResponse(unittest.TestCase):
+    def test_run_by_accepted_keyword(self):
+        event = create_sample_app_mention_event("<@U01BN035Y6L> 섞어줘 <@Uabc> <@Udef>")
+        mock_slack_client = MagicMock()
+        shuffle = ShuffleResponse(event, mock_slack_client)
+        fallback = SimpleResponse(event, mock_slack_client)
+        sut = MentionResponse([shuffle], fallback)
+
+        result = sut.run()
+
+        mock_slack_client.send_message.assert_called_once()
+        assert result is True
+        assert mock_slack_client.send_message.call_args.kwargs["msg"] in ["여기있어!\n<@Uabc> <@Udef>", "여기있어!\n<@Udef> <@Uabc>"]
+
+    def test_run_by_unaccepted_keyword_by_fallback(self):
+        event = create_sample_app_mention_event("<@U01BN035Y6L>")
+        mock_slack_client = MagicMock()
+        shuffle = ShuffleResponse(event, mock_slack_client)
+        fallback = SimpleResponse(event, mock_slack_client)
+        sut = MentionResponse([shuffle], fallback)
+
+        result = sut.run()
+
+        assert result is True
+        assert mock_slack_client.send_message.call_args.kwargs["msg"] == "?"
